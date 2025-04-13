70ccf50a73b5768db7aed64bb0869c6904df8976
Abinesh Joyel <abinesh@sodisys.io>
2025-04-13 12:21:03 +0530

    deploy script update

--- scripts/deploy-client.js
+++ scripts/deploy-client.js
@@ -10,20 +10,21 @@ const __dirname = dirname(__filename);
 const DIST_DIR = join(__dirname, '../apps/glide-ui-client/dist');
 const TEMP_DIR = join(__dirname, '.gh-pages-temp');
 const BRANCH = 'gh-pages';
-
 const run = cmd => {
   console.log(`\n$ ${cmd}`);
   execSync(cmd, { stdio: 'inherit' });
 };
 
 try {
+  // Get the remote URL of the current Git repo
+  const remoteUrl = execSync('git config --get remote.origin.url')
+    .toString()
+    .trim();
   // 1. Clean up old temp directory
   run(`rm -rf ${TEMP_DIR}`);
 
   // 2. Clone gh-pages branch into temp directory
-  run(
-    `git clone -b ${BRANCH} --single-branch $(git config --get remote.origin.url) ${TEMP_DIR}`
-  );
+  run(`git clone -b ${BRANCH} --single-branch ${remoteUrl} ${TEMP_DIR}`);
 
   // 3. Clear the old contents
   run(`rm -rf ${TEMP_DIR}/*`);
