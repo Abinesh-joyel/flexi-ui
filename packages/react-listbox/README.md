de948ba2e539ce5b575823afeb5d105b020254a6
Abinesh Joyel <abinesh@sodisys.io>
2025-04-25 21:39:25 +0530

    react listbox setup

---
+++ packages/react-listbox/README.md
@@ -0,0 +1,3 @@
+# @glide-ui/react-listbox
+
+An accessible Listbox component for React applications, designed to provide custom select functionality with full WAI-ARIA compliance.
---
+++ packages/react-listbox/vite.config.ts
@@ -0,0 +1,48 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { dirname, resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import dts from 'vite-plugin-dts';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.tsx'),
+      name: '@glide-ui/react-listbox',
+      fileName: 'index',
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['react', 'react-dom', 'react/jsx-runtime'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+          'react/jsx-runtime': 'jsxRuntime',
+        },
+      },
+    },
+  },
+  plugins: [
+    react(),
+    dts({
+      insertTypesEntry: true,
+      include: ['src'],
+      exclude: ['src/main.tsx', 'src/__tests__'],
+      rollupTypes: false,
+    }),
+  ],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: 'modern-compiler',
+      },
+    },
+  },
+});
